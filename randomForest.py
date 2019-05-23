03b1508eeef4155c66e81075ae3d213fa90fcc2f
jorgeality <jorgeality29@gmail.com>
2019-05-23 15:59:17 -0500

    modelos

--- randomForest.py
+++ randomForest.py
@@ -6,53 +6,51 @@ import scipy.stats as st
 import sqlite3
 
 bd = sqlite3.connect("./salesdb/salesDB_grocery_market/restaurante.sqlite")
+print('conexion exitosa!')
 dataB = bd.cursor()
-allData = dataB.execute('select  categories.CategoryName, Products.ProductName, sales.Quantity, sales.SalesDate from sales, Products, categories where sales.ProductID = Products.ProductID AND categories.CategoryID = Products.CategoryID LIMIT 10')
+allData = dataB.execute('select categories.CategoryName, Products.ProductName, sales.Quantity, Products.Price,(sales.Quantity*Products.Price), sales.SalesDate from sales, Products, categories where sales.ProductID = Products.ProductID AND categories.CategoryID = Products.CategoryID LIMIT 100000')
+products = []
+print('leyendo datos!')
 for row in allData:
-    print(row) 
-'''
-# función para obtener estadísticas sobre los datos
-def valInterval(g):
-    st_interval = st.t.interval(
-        0.95,
-        len(g) - 1,
-        loc=np.mean(g.Quantity),
-        scale=st.sem(g.Quantity))
-    return pd.Series(dict(st_interval=st_interval))
+    products.append({
+        'Category': row[0],
+        'Product': row[1],
+        'Quantity': row[2],
+        'Price': row[3],
+        'TotalPrice': row[4],
+        'SalesDate': row[5]
+    })
+data = pd.DataFrame(data=products)
+print('lectura exitosa!')
 
-
-# importando el dataset
-data = pd.read_csv(
-    "./salesdb/salesDB_grocery_market/sales.csv",
-    sep=";",
-    nrows=150000,
-    usecols=["Quantity", "SalesDate","ProductID"])
-
-dataProd = pd.read_csv(
-    "./salesdb/salesDB_grocery_market/products.csv",
-    sep=";",
-    nrows=150000,
-    usecols=["ProductID", "ProductName","Price", "CategoryID"])
-dataCat = pd.read_csv(
-    "./salesdb/salesDB_grocery_market/categories.csv",
-    sep=";",
-    nrows=150000,
-    usecols=["CategoryID", "CategoryName"])
 # convirtiendo la columna de fecha en datetime
+data = data.sort_values("SalesDate")
+print('organizando datos')
+data = data[data["SalesDate"]!='NULL']
+print('eliminando datos nulos')
+
 data["SalesDate"] = pd.to_datetime(
     data["SalesDate"], format="%Y-%m-%d %H:%M:%S.%f")
-
+print('fechas parseadas')
 # creando columnas de fecha y hora
 data["fecha"] = data["SalesDate"].dt.date
 data["hora"] = data["SalesDate"].dt.hour
+data["dia"] = data["SalesDate"].dt.dayofweek
 
 # agrupando por fecha y hora
-data = data.groupby(["ProductID", "fecha", "hora"]).mean()
+data = data.groupby(["Product","hora","dia", "fecha", "Price"]).mean()
+print('datos agrupados')
 #data = data.groupby([ data["hora"], data["ProductID"]]).sum()
 
 # se promedia el total de compras por cada hora del día en todos los días
 # del dataset
-data = data.reset_index(level="ProductID")
+data = data.reset_index()
 #data = data.groupby(data.hora).mean()
-print(data)
+print(data["Product"])
+
+'''
+- total ventas en la semana
+- que producto se vende mas en cierto dia
+- 
+
 '''
