e05bf8d00e9cfecfbb2bde0890969c9872658d80
Luischv <luischa123@gmail.com>
2019-05-23 01:07:50 -0500

    Se agregan dos archivos al proyecto.
    z

---
+++ polinomico.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+data = pd.read_csv(
+    "./salesdb/salesDB_grocery_market/sales.csv",
+    sep=";",
+    nrows=50000,
+    usecols=["Quantity", "SalesDate"])
+
+data["SalesDate"] = pd.to_datetime(
+    data["SalesDate"], format="%Y-%m-%d %H:%M:%S.%f")
+
+data = data.groupby(data["SalesDate"].dt.hour).sum()
+data["index1"] = data.index
+
+# graficando
+grouped2 = data
+grouped2["hora"] = data["SalesDate"].apply(lambda x: x.hour)
+grouped2["dia"] = grouped2["SalesDate"].apply(lambda x: x.day)
+grouped2 = grouped2.groupby(["dia", "hora"]).sum()
+grouped2.plot(ls="none", marker="o")
+
+Y = data["Quantity"]
+X = data.drop("Quantity", axis=1)
+# YY = grouped2["Quantity"]
+# XX = grouped2.drop("Quantity", axis=1)
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(
+    X, Y, test_size=0.2, random_state=123)
+
+linear_regression = LinearRegression()
+polynomial_features = PolynomialFeatures(degree=1, include_bias=False)
+
+pipeline = Pipeline([("polynomial_features", polynomial_features),
+                     ("linear_regression", linear_regression)])
+pipeline.fit(Xtrain, Ytrain)
+
+randomForest = RandomForestRegressor()
+randomForest.fit(Xtrain, Ytrain)
+
+print(r2_score(Ytest, randomForest.predict(Xtest)))
+
+scores = cross_validation.cross_val_score(
+    pipeline, X, Y, scoring="mean_squared_error", cv=10)
+
+
+
+print(r2_score(Ytest, pipeline.predict(Xtest)))
---
+++ randomForest.py
@@ -0,0 +1,42 @@
+# importando las bibliotecas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as st
+
+
+# función para obtener estadísticas sobre los datos
+def valInterval(g):
+    st_interval = st.t.interval(
+        0.95,
+        len(g) - 1,
+        loc=np.mean(g.Quantity),
+        scale=st.sem(g.Quantity))
+    return pd.Series(dict(st_interval=st_interval))
+
+
+# importando el dataset
+data = pd.read_csv(
+    "./salesdb/salesDB_grocery_market/sales.csv",
+    sep=";",
+    nrows=150000,
+    usecols=["Quantity", "SalesDate"])
+
+# convirtiendo la columna de fecha en datetime
+data["SalesDate"] = pd.to_datetime(
+    data["SalesDate"], format="%Y-%m-%d %H:%M:%S.%f")
+
+# creando columnas de fecha y hora
+data["fecha"] = data["SalesDate"].dt.date
+data["hora"] = data["SalesDate"].dt.hour
+
+# agrupando por fecha y hora
+data = data.groupby([data["fecha"], data["hora"]]).sum()
+
+# se promedia el total de compras por cada hora del día en todos los días
+# del dataset
+data = data.reset_index(level="hora")
+data = data.groupby(data.hora).mean()
+
+
+# se calculan los márgenes de confianza para este promedio
