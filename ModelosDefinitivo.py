28199726733d944c8076e7374a219507f830a8a4
Luischv <luischa123@gmail.com>
2019-05-24 03:37:44 -0500

    se añaden gráficos de modelos

--- ModelosDefinitivo.py
+++ ModelosDefinitivo.py
@@ -1,6 +1,9 @@
 # DataFrame
 import pandas as pd
 
+# para graficar
+import matplotlib.pyplot as plt
+
 # ### modelos de sklearn
 from sklearn import model_selection as ms
 from sklearn.linear_model import LinearRegression
@@ -58,3 +61,17 @@ for name, model in modelos:
     names.append(name)
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
+
+# gráficas de los modelos con los puntos
+linear = LinearRegression().fit(X, Y)
+decision_tree = DecisionTreeRegressor().fit(X, Y)
+random_forest = RandomForestRegressor().fit(X, Y)
+svr = SVR().fit(X, Y)
+
+plt.scatter(X, Y)
+plt.plot(datosGlobal.dia.sort_values(), linear.predict(datosGlobal.dia.sort_values().values.reshape(-1, 1)), label="lineal")
+plt.plot(datosGlobal.dia.sort_values(), decision_tree.predict(datosGlobal.dia.sort_values().values.reshape(-1, 1)), label="arbol de decision")
+plt.plot(datosGlobal.dia.sort_values(), random_forest.predict(datosGlobal.dia.sort_values().values.reshape(-1, 1)), label="bosque aleatorio")
+plt.plot(datosGlobal.dia.sort_values(), svr.predict(datosGlobal.dia.sort_values().values.reshape(-1, 1)), label="maquina de soporte de vectores")
+
+plt.legend(loc="best")
