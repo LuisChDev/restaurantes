20c16770168b82ceecc27e1469dfc2f991893c9a
jorgeality <jorgeality29@gmail.com>
2019-05-23 23:54:57 -0500

    1 data frame entregado

--- randomForest.py
+++ randomForest.py
@@ -5,52 +5,74 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 import sqlite3
 
-bd = sqlite3.connect("./salesdb/salesDB_grocery_market/restaurante.sqlite")
-print('conexion exitosa!')
-dataB = bd.cursor()
-allData = dataB.execute('select categories.CategoryName, Products.ProductName, sales.Quantity, Products.Price,(sales.Quantity*Products.Price), sales.SalesDate from sales, Products, categories where sales.ProductID = Products.ProductID AND categories.CategoryID = Products.CategoryID LIMIT 100000')
-products = []
-print('leyendo datos!')
-for row in allData:
-    products.append({
-        'Category': row[0],
-        'Product': row[1],
-        'Quantity': row[2],
-        'Price': row[3],
-        'TotalPrice': row[4],
-        'SalesDate': row[5]
-    })
-data = pd.DataFrame(data=products)
-print('lectura exitosa!')
+def dataset():
+    bd = sqlite3.connect("./salesdb/salesDB_grocery_market/restaurante.sqlite")
+    print('conexion exitosa!')
+    dataB = bd.cursor()
+    allData = dataB.execute('select categories.CategoryName, Products.ProductName, sales.Quantity, Products.Price,(sales.Quantity*Products.Price), sales.SalesDate from sales, Products, categories where sales.ProductID = Products.ProductID AND categories.CategoryID = Products.CategoryID LIMIT 100000')
+    products = []
+    print('leyendo datos!')
+    for row in allData:
+        products.append({
+            'Category': row[0],
+            'Product': row[1],
+            'Quantity': row[2],
+            'Price': row[3],
+            'TotalPrice': row[4],
+            'SalesDate': row[5]
+        })
+    data = pd.DataFrame(data=products)
+    print('lectura exitosa!')
+    return data
 
-# convirtiendo la columna de fecha en datetime
-data = data.sort_values("SalesDate")
-print('organizando datos')
-data = data[data["SalesDate"]!='NULL']
-print('eliminando datos nulos')
+def limpiandoDataset(data):
 
-data["SalesDate"] = pd.to_datetime(
-    data["SalesDate"], format="%Y-%m-%d %H:%M:%S.%f")
-print('fechas parseadas')
-# creando columnas de fecha y hora
-data["fecha"] = data["SalesDate"].dt.date
-data["hora"] = data["SalesDate"].dt.hour
-data["dia"] = data["SalesDate"].dt.dayofweek
+    # convirtiendo la columna de fecha en datetime
+    data = data.sort_values("SalesDate")
+    print('organizando datos')
+    data = data[data["SalesDate"]!='NULL']
+    print('eliminando datos nulos')
+    data["SalesDate"] = pd.to_datetime(
+        data["SalesDate"], format="%Y-%m-%d %H:%M:%S.%f")
+    print('fechas parseadas')
+    # creando columnas de fecha y hora
+    data["fecha"] = data["SalesDate"].dt.date
+    data["hora"] = data["SalesDate"].dt.hour
+    data["dia"] = data["SalesDate"].dt.dayofweek
+    return data
 
-# agrupando por fecha y hora
-data = data.groupby(["Product","hora","dia", "fecha", "Price"]).mean()
-print('datos agrupados')
-#data = data.groupby([ data["hora"], data["ProductID"]]).sum()
+def agruparDatosSUM(data, campos, campo):
+    data = data.groupby(campos).sum()
+    print('datos agrupados')
+    #data = data.groupby([ data["hora"], data["ProductID"]]).sum()
 
-# se promedia el total de compras por cada hora del día en todos los días
-# del dataset
-data = data.reset_index()
-#data = data.groupby(data.hora).mean()
-print(data["Product"])
+    # se promedia el total de compras por cada hora del día en todos los días
+    # del dataset
+    if campo != "":
+        data = data.reset_index(level=campo)
+    else:
+        data = data.reset_index()
+    #data = data.groupby(data.hora).mean()
+    print(data)
+    return data
 
-'''
-- total ventas en la semana
-- que producto se vende mas en cierto dia
-- 
+def agruparDatosMean(data, campos, campo):
+    data = data.groupby(campos).mean()
+    print('datos agrupados')
+    #data = data.groupby([ data["hora"], data["ProductID"]]).sum()
 
-'''
+    # se promedia el total de compras por cada hora del día en todos los días
+    # del dataset
+    if campo != "":
+        data = data.reset_index(level=campo)
+    else:
+        data = data.reset_index()
+    #data = data.groupby(data.hora).mean()
+    print(data)
+    return data
+
+
+if __name__== "__main__":
+    data1 = dataset()
+    data1 = limpiandoDataset(data1)
+    data1 = agruparDatosSUM(data1, ["Product", "dia", "fecha", "Price"],"")
