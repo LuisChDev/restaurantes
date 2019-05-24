e20e1fac335512bd6d75e0315ef429d7f14aba6f
jorgeality <jorgeality29@gmail.com>
2019-05-24 00:24:38 -0500

    1 data frame entregado

--- randomForest.py
+++ randomForest.py
@@ -39,6 +39,8 @@ def limpiandoDataset(data):
     data["fecha"] = data["SalesDate"].dt.date
     data["hora"] = data["SalesDate"].dt.hour
     data["dia"] = data["SalesDate"].dt.dayofweek
+    data["semana"] = data["SalesDate"].dt.week
+
     return data
 
 def agruparDatosSUM(data, campos, campo):
@@ -68,11 +70,44 @@ def agruparDatosMean(data, campos, campo):
     else:
         data = data.reset_index()
     #data = data.groupby(data.hora).mean()
-    print(data)
+    
     return data
 
 
 if __name__== "__main__":
-    data1 = dataset()
-    data1 = limpiandoDataset(data1)
-    data1 = agruparDatosSUM(data1, ["Product", "dia", "fecha", "Price"],"")
+
+    # dataset 1 - productos vendidos en un dia
+    #data1 = dataset()
+    #data1 = limpiandoDataset(data1)
+    #data1 = agruparDatosSUM(data1, ["Product", "dia", "fecha", "Price"],"")
+    #print(data1)
+    
+    # dataset 2 - cantidad de un cierto producto vendido en cierta semana
+    #data2 = dataset()
+    #data2 = limpiandoDataset(data2)
+    #data2 = agruparDatosSUM(data2, ["Product", "semana", "Price"],"")
+    #print(data2)
+
+    # dataset 3 - ganancias generadas en cierta semana
+    #data3 = dataset()
+    #data3 = limpiandoDataset(data3)
+    #data3 = agruparDatosSUM(data3, ["semana"],"")
+    #print(data3)
+
+    # dataset 4 - ganancias generadas en cierto dia de la semana
+    #data4 = dataset()
+    #data4 = limpiandoDataset(data4)
+    #data4 = agruparDatosSUM(data4, ["dia"],"")
+    #print(data4)
+
+    # dataset 5 - ganancias generada por los productos
+    #data5 = dataset()
+    #data5 = limpiandoDataset(data5)
+    #data5 = agruparDatosSUM(data5, ["Product"],"")
+    #print(data5)
+
+    # dataset 6 - ganancias generada por los categorias
+    data6 = dataset()
+    data6 = limpiandoDataset(data6)
+    data6 = agruparDatosSUM(data6, ["Category"],"")
+    print(data6)
